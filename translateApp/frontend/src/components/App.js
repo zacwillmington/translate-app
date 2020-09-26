19e2f3127e806f5c3c64c7ad55f559b739d302f2
Zac Willmington <zac.willmington@gmail.com>
2020-09-26 13:01:05 -0500

    build out and connected frontend.

---
+++ translateApp/frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Header from './layout/Header';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <Header></Header>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
